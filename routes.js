292fab7b798309f5f5521a464e4d7072a2c3e1b5
xiaoshitou <zhuliminl@gmail.com>
2018-03-12 01:11:35 +0800

    去掉后端的全局路由匹配，改用前端的重定向页面

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     // app.use('/comments', require('./api/comment'));              // 弃用。改在 /posts 路由下实现
 
 
-    app.use('/*', function(req, res, next) {
-        res.status(200).send({ message: '请求虽然成功，但是大概不是你想要的'})
-    })
+    // app.use('/*', function(req, res, next) {
+        // res.status(200).send({ message: '请求虽然成功，但是大概不是你想要的'})
+    // })
 }
