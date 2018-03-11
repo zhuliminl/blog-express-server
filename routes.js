08ac49747715368752851fae68a4f833b8b4a72c
xiaoshitou <zhuliminl@gmail.com>
2018-03-12 01:31:23 +0800

    修改后端兜底的路由，指向前端的首页

--- routes.js
+++ routes.js
@@ -7,7 +7,9 @@ module.exports = function(app) {
     // app.use('/comments', require('./api/comment'));              // 弃用。改在 /posts 路由下实现
 
 
-    // app.use('/*', function(req, res, next) {
+    app.use('/*', function(req, res, next) {
         // res.status(200).send({ message: '请求虽然成功，但是大概不是你想要的'})
-    // })
+        // 单页应用中，前端路由是自行渲染的。浏览器一刷新，接下来加载的就是后端的路由。所以应该将后端的兜底的路由重定向到前端的首页，保持前端有内容
+        res.redirect('/');
+    })
 }
