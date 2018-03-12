16471f3bbf45e98b6c4b7e48905b1b87dd656846
xiaoshitou <zhuliminl@gmail.com>
2018-03-12 17:28:16 +0800

    后续优化更新

--- api/register/index.js
+++ api/register/index.js
@@ -12,9 +12,9 @@ const db = require('../../models');
 const { User } = db;
 const SECRET = 'skdjflksdjflksjdlkfjskldjflksdjflksdjkfjsdkf';
 // 各字段的匹配正则，后期都应该放到配置文件中
-const regUsername = /^[a-zA-Z0-9_-]{4,16}$/,
+const regUsername = /^[\u0391-\uFFE5]+$/,                // 匹配一个或者多个汉子
       regEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
-      regPassword = /^.*(?=.{6,})/;
+      regPassword = /^[a-zA-Z0-9]{6,16}$/;                 // 以字母开头，长度在 6-16 之间
 
 
 module.exports = register;
@@ -25,7 +25,7 @@ async function register(req, res) {
     try {
         const user = await User.findOne({ where: { email: email } });
         if(user) {
-            return res.status(400).send({ message: '该用户已经被占用'})
+            return res.status(400).send({ message: '该邮箱已经被占用', errorType: 'email'})
         }
 
     } catch(err) {
@@ -36,17 +36,17 @@ async function register(req, res) {
 
         // 验证字段
         if(!regUsername.test(username)) {
-            return res.status(400).send({ message: '用户名不符合要求' });
+            return res.status(400).send({ message: '用户名不符合要求', errorType: 'username' });
         };
         if(!regEmail.test(email)) {
-            return res.status(400).send({ message: '邮箱不符合要求' });
+            return res.status(400).send({ message: '邮箱不符合要求', errorType: 'email' });
         };
         if(!regPassword.test(password)) {
-            return res.status(400).send({ message: '密码不符合要求'});
+            return res.status(400).send({ message: '密码不符合要求', errorType: 'password'});
         }
 
     } else {
-        return res.status(400).send({ message: '字段不完整' })
+        return res.status(400).send({ message: '字段不完整，无法提交', errorType: 'fieldlack' })
     }
 
     const newUser = {
